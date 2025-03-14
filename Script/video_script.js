90c34d7a06628431eeaaff976f875be0cd8ff43a
Jyo J <100463025+Fossa808@users.noreply.github.com>
2025-03-13 23:35:31 -0700

    Made changes to the code for API

--- Script/video_script.js
+++ Script/video_script.js
@@ -15,14 +15,22 @@ function makeRequest(){
     });
     request.execute(function(response)  {                                                                                    
             $('#results').empty()
-            var srchItems = response.result.items;                      
+            var srchItems = response.result.items;
+            var videoTypeId = response.result.items[0].id;
+            var videoSnippet = response.result.items[0].snippet;
+            var videoId = response.result.items[0].id.videoId;
+            console.log("ID: " + videoTypeId);
+            console.log("Snippet: " + videoSnippet);
+            console.log("Video ID: " + videoId);                
             $.each(srchItems, function(index, item){
-            vidTitle = item.snippet.title;  
+            vidTitle = item.snippet.title;
+            //while()
+
+            var vidUrl = `https://www.youtube.com/watch?v=${videoId}`;
             vidThumburl =  item.snippet.thumbnails.default.url;                 
             vidThumbimg = '<pre><img id="thumb" src="'+
-            vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
-
-            $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
+            vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';
+            $('#results').append('<pre>' + vidTitle + vidThumbimg + vidUrl + '</pre>');                      
     })  
   })  
 }
